9f41df60f7c2568e76ad7e8617d534daa000f196
gro-mikhail <hramyka.m@yandex.ru>
2023-01-03 23:33:59 +0300

    second part done

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,14 +1,33 @@
 # frozen_string_literal: true
 
 require_relative 'rulers/version'
-require 'rulers/array'
+require 'rulers/routing'
 
 module Rulers
   class Application
     def call(env)
-      `echo debug > debug.txt`;
-      [200, {'Content-Type' => 'text/html'},
-       ["Hello from Ruby on Rulers!"]]
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, { 'Content-Type' => 'text/html' }, []]
+      end
+
+      env['PATH_INFO'] = '/home/index' if env['PATH_INFO'] == '/'
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      begin
+        text = controller.send(act)
+      rescue StandardError
+        return [500, { 'Content-Type' => 'text/html' }, ['Something went wrong!']]
+      end
+      [200, { 'Content-Type' => 'text/html' }, [text]]
+    end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
     end
   end
 end
